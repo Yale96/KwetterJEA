7226c8633fa01295dc9baab353068aa997d63398
Yale96 <yannickvanleeuwen@i-lion.nl>
2018-03-13 07:40:35 +0100

    all rest assured tests

---
+++ Code/Kwetter/src/test/java/RestTests/UserRestTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Yannick van Leeuwen
+ */
+public class UserRestTest {
+    
+    //        public static void setup() {
+//        String port = System.getProperty("server.port");
+//        if (port == null) {
+//            RestAssured.port = Integer.valueOf(8080);
+//        }
+//        else{
+//            RestAssured.port = Integer.valueOf(port);
+//        }
+//
+//        String basePath = System.getProperty("server.base");
+//        if(basePath==null){
+//            basePath = "/Kwetter/resources/";
+//        }
+//        RestAssured.basePath = basePath;
+//
+//        String baseHost = System.getProperty("server.host");
+//        if(baseHost==null){
+//            baseHost = "http://localhost";
+//        }
+//        RestAssured.baseURI = baseHost;
+//    }
+//    
+//    @Test
+//    public void basicPingTest() {
+//        given().when().get("users").then().statusCode(200);
+//    }
+}

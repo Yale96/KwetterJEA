dd560a8703ac54ec9f6073c3c077ea33550ba45f
Yannick <yannickvanleeuwen@i-lion.nl>
2018-03-20 14:44:19 +0100

    begin aan de frontend

---
+++ Code/Kwetter/src/main/java/ManagedBeansForJsf/UserView.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBeansForJsf;
+
+import Models.User;
+import Services.UserService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author yanni
+ */
+@ManagedBean(name="userView")
+@ViewScoped
+public class UserView {
+    
+    @Inject
+    private UserService userService;
+    
+    List<User> users;
+    /**
+     * Creates a new instance of UserView
+     */
+    public UserView() {
+    }
+    
+    public List<User> getAllUsers()
+    {
+        return userService.getUsers();
+    }
+    
+}
